545f177228cd911dd9e43fd3793100007ed6724c
Karl Umberto Kats <karl.pongolini@gmail.com>
2023-05-25 18:01:15 +0300

    Rename placeholder content

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,20 +4,20 @@
             @csrf
             <input type="text" name="name"
                     value="{{ old('name') }}"
-                    placeholder="{{ __('Name the service') }}"
+                    placeholder="{{ __('Name the product') }}"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
             <input type="number" name="procurementPrice_cents"
                     value="{{ old('procurementPrice_cents') }}"
-                    placeholder="{{ __('Base price in cents') }}"
+                    placeholder="{{ __('Procurement price in cents') }}"
                     class="mt-2 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
             <x-input-error :messages="$errors->get('procurementPrice_cents')" class="mt-2" />
             <x-input-error :messages="$errors->get('duration_minutes')" class="mt-2" />
             <textarea name="description"
-                    placeholder="{{ __('Add a description for the service.') }}"
+                    placeholder="{{ __('Add a description for the product.') }}"
                 class="mt-2 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">{{ old('description') }}</textarea>
             <x-input-error :messages="$errors->get('description')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Add Service') }}</x-primary-button>
+            <x-primary-button class="mt-4">{{ __('Add Product') }}</x-primary-button>
         </form>
     </div>
 </x-app-layout>
